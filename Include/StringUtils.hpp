c775309d6f346735bec75ff0601a191ee1cdc6ca
Hani Ammar <hani.code@outlook.com>
2020-10-19 00:58:31 +0300

    Clean up

--- Include/StringUtils.hpp
+++ Include/StringUtils.hpp
@@ -36,9 +36,9 @@ struct StringUtils {
         static constexpr Char_T_ CarriageControlChar = '\r';
     };
 
-    template <typename Char_T_, typename NumberType = SizeT>
-    static NumberType Count(const Char_T_ *str) noexcept {
-        NumberType len = 0;
+    template <typename Char_T_, typename Number_T_ = SizeT>
+    static Number_T_ Count(const Char_T_ *str) noexcept {
+        Number_T_ len = 0;
 
         if (str != nullptr) {
             while (str[len] != 0) {
@@ -49,35 +49,43 @@ struct StringUtils {
         return len;
     }
 
-    template <typename Char_T_, typename Type_>
-    static void StartTrim(const Char_T_ *str, Type_ &offset,
-                          Type_ end_before) noexcept {
+    template <typename Char_T_, typename Number_T_>
+    static void StartTrim(const Char_T_ *str, Number_T_ &offset,
+                          Number_T_ end_before) noexcept {
         using WhiteSpaceChars_T_ = WhiteSpaceChars<Char_T_>;
 
-        while ((offset < end_before) &&
-               ((str[offset] == WhiteSpaceChars_T_::SpaceChar) ||
-                (str[offset] == WhiteSpaceChars_T_::LineControlChar) ||
-                (str[offset] == WhiteSpaceChars_T_::TabControlChar) ||
-                (str[offset] == WhiteSpaceChars_T_::CarriageControlChar))) {
+        while (offset < end_before) {
+            const Char_T_ c = str[offset];
+
+            if ((c != WhiteSpaceChars_T_::SpaceChar) &&
+                (c != WhiteSpaceChars_T_::LineControlChar) &&
+                (c != WhiteSpaceChars_T_::TabControlChar) &&
+                (c != WhiteSpaceChars_T_::CarriageControlChar)) {
+                return;
+            }
+
             ++offset;
         }
     }
 
-    template <typename Char_T_, typename Type_>
-    static void SoftTrim(const Char_T_ *str, Type_ &offset,
-                         Type_ &length) noexcept {
+    template <typename Char_T_, typename Number_T_>
+    static void SoftTrim(const Char_T_ *str, Number_T_ &offset,
+                         Number_T_ &length) noexcept {
         using WhiteSpaceChars_T_ = WhiteSpaceChars<Char_T_>;
 
         if (length != 0) {
-            Type_ end_before = (length + offset);
+            Number_T_ end_before = (length + offset);
             StartTrim(str, offset, end_before);
 
-            while ((--end_before > offset) &&
-                   ((str[end_before] == WhiteSpaceChars_T_::SpaceChar) ||
-                    (str[end_before] == WhiteSpaceChars_T_::LineControlChar) ||
-                    (str[end_before] == WhiteSpaceChars_T_::TabControlChar) ||
-                    (str[end_before] ==
-                     WhiteSpaceChars_T_::CarriageControlChar))) {
+            while (--end_before > offset) {
+                const Char_T_ c = str[end_before];
+
+                if ((c != WhiteSpaceChars_T_::SpaceChar) &&
+                    (c != WhiteSpaceChars_T_::LineControlChar) &&
+                    (c != WhiteSpaceChars_T_::TabControlChar) &&
+                    (c != WhiteSpaceChars_T_::CarriageControlChar)) {
+                    break;
+                }
             }
 
             ++end_before;
