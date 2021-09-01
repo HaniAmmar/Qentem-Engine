22ad2f94903b8b16cad88b6f1fdbf2545f172dbb
Hani Ammar <hani.code@outlook.com>
2021-09-01 14:14:13 +0300

    Update ALE example 1

--- Examples/ALE/ALE01.cpp
+++ Examples/ALE/ALE01.cpp
@@ -4,35 +4,35 @@
 #include <iostream>
 
 int main() {
-    Qentem::Array<const char *> math;
+    Qentem::Array<const char *> equations;
 
-    math += "+1+1";                         // 2
-    math += "---1";                         // -1
-    math += "-1+3";                         // 2
-    math += "-1*8";                         // -8
-    math += "--1*5";                        // 5
-    math += "1--1";                         // 2
-    math += "11==11";                       // 1
-    math += "11!=11";                       // 0
-    math += "11>2";                         // 1
-    math += "11>=2";                        // 1
-    math += "1<2";                          // 1
-    math += "2^8";                          // 256
-    math += "11<=11";                       // 1
-    math += "11&&2";                        // 1
-    math += "4*2+6/3";                      // 10
-    math += "(2+3)*5";                      // 25
-    math += "(5)+3";                        // 8
-    math += "((1+2)^(1+2))/2";              // 13.5
-    math += "((5/5+1)*2+1)+3*3";            // 14
-    math += "5+2*4-8/2==9 && 1";            // 1
-    math += "((5/5+1)*2+1)+3*3 != 12 && 1"; // 1
-    math +=
+    equations += "+1+1";                         // 2
+    equations += "---1";                         // -1
+    equations += "-1+3";                         // 2
+    equations += "-1*8";                         // -8
+    equations += "--1*5";                        // 5
+    equations += "1--1";                         // 2
+    equations += "11==11";                       // 1
+    equations += "11!=11";                       // 0
+    equations += "11>2";                         // 1
+    equations += "11>=2";                        // 1
+    equations += "1<2";                          // 1
+    equations += "2^8";                          // 256
+    equations += "11<=11";                       // 1
+    equations += "11&&2";                        // 1
+    equations += "4*2+6/3";                      // 10
+    equations += "(2+3)*5";                      // 25
+    equations += "(5)+3";                        // 8
+    equations += "((1+2)^(1+2))/2";              // 13.5
+    equations += "((5/5+1)*2+1)+3*3";            // 14
+    equations += "5+2*4-8/2==9 && 1";            // 1
+    equations += "((5/5+1)*2+1)+3*3 != 12 && 1"; // 1
+    equations +=
         R"(2  * 1 * 3 + 1 - 4 + (10 - 5 - 6 + 1 + 1 + 1 + 1) *
         (8 / 4 + 1) - 1 - -1 + 2 == ((5/5+1)*2+1)+3*3)"; // 1
     ///////////////////////////////////////////////////
 
-    for (unsigned int i = 0; i < math.Size(); i++) {
-        std::cout << Qentem::ALE::Evaluate(math[i]) << '\n';
+    for (auto eq = equations.First(), end = equations.End(); eq != end; eq++) {
+        std::cout << Qentem::ALE::Evaluate(*eq) << '\n';
     }
 }
