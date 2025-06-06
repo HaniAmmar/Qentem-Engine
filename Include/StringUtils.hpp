ab58e8775e97a303965f7cd99a8a62a06c2f3a69
HaniAmmar <hani.code@outlook.com>
2025-06-06 17:52:39 +0300

    Change 'StringUtils' from 'namespace' to 'struct'

--- Include/StringUtils.hpp
+++ Include/StringUtils.hpp
@@ -18,59 +18,38 @@
 
 namespace Qentem {
 
-namespace StringUtils {
-template <typename Char_T>
-struct WhiteSpaceChars_T {
-    static constexpr Char_T SpaceChar           = ' ';
-    static constexpr Char_T LineControlChar     = '\n';
-    static constexpr Char_T TabControlChar      = '\t';
-    static constexpr Char_T CarriageControlChar = '\r';
-};
+template <typename, SizeT32>
+struct HTMLSpecialChars_T {};
 
-template <typename Char_T, typename Number_T = SizeT>
-static QENTEM_CONST_EXPRESSION Number_T Count(const Char_T *str) noexcept {
-    Number_T length = 0;
+struct StringUtils {
+    template <typename Char_T>
+    struct WhiteSpaceChars_T {
+        static constexpr Char_T SpaceChar           = ' ';
+        static constexpr Char_T LineControlChar     = '\n';
+        static constexpr Char_T TabControlChar      = '\t';
+        static constexpr Char_T CarriageControlChar = '\r';
+    };
 
-    if (str != nullptr) {
-        while (*str != Char_T{0}) {
-            ++str;
-            ++length;
-        }
-    }
+    template <typename Char_T, typename Number_T = SizeT>
+    static QENTEM_CONST_EXPRESSION Number_T Count(const Char_T *str) noexcept {
+        Number_T length = 0;
 
-    return length;
-}
-
-template <typename Char_T, typename Number_T>
-static void TrimLeft(const Char_T *str, Number_T &offset, const Number_T end_offset) noexcept {
-    using WhiteSpaceChars = WhiteSpaceChars_T<Char_T>;
-
-    while (offset < end_offset) {
-        switch (str[offset]) {
-            case WhiteSpaceChars::SpaceChar:
-            case WhiteSpaceChars::LineControlChar:
-            case WhiteSpaceChars::TabControlChar:
-            case WhiteSpaceChars::CarriageControlChar:
-                break;
-
-            default: {
-                return;
+        if (str != nullptr) {
+            while (*str != Char_T{0}) {
+                ++str;
+                ++length;
             }
         }
 
-        ++offset;
+        return length;
     }
-}
 
-template <typename Char_T, typename Number_T>
-static void TrimRight(const Char_T *str, const Number_T offset, Number_T &end_offset) noexcept {
-    using WhiteSpaceChars = WhiteSpaceChars_T<Char_T>;
+    template <typename Char_T, typename Number_T>
+    static void TrimLeft(const Char_T *str, Number_T &offset, const Number_T end_offset) noexcept {
+        using WhiteSpaceChars = WhiteSpaceChars_T<Char_T>;
 
-    if (end_offset > offset) {
-        do {
-            --end_offset;
-
-            switch (str[end_offset]) {
+        while (offset < end_offset) {
+            switch (str[offset]) {
                 case WhiteSpaceChars::SpaceChar:
                 case WhiteSpaceChars::LineControlChar:
                 case WhiteSpaceChars::TabControlChar:
@@ -78,207 +57,229 @@ static void TrimRight(const Char_T *str, const Number_T offset, Number_T &end_of
                     break;
 
                 default: {
-                    ++end_offset;
                     return;
                 }
             }
 
-        } while (end_offset > offset);
-    }
-}
-
-// offset: the starting offset
-// length: the number of characters
-template <typename Char_T, typename Number_T>
-static void Trim(const Char_T *str, Number_T &offset, Number_T &length) noexcept {
-    if (length != 0) {
-        Number_T end_offset = (length + offset);
-        TrimLeft(str, offset, end_offset);
-        TrimRight(str, offset, end_offset);
-        length = (end_offset - offset);
-    }
-}
-
-template <typename Char_T>
-static bool IsLess(const Char_T *left, const Char_T *right, SizeT left_length, SizeT right_length,
-                   bool orEqual) noexcept {
-    SizeT offset = 0;
-
-    while ((left_length > offset) && (right_length > offset)) {
-        if (left[offset] > right[offset]) {
-            return false;
+            ++offset;
         }
-
-        if (left[offset] < right[offset]) {
-            return true;
-        }
-
-        ++offset;
     }
 
-    return (orEqual & (left_length == right_length));
-}
+    template <typename Char_T, typename Number_T>
+    static void TrimRight(const Char_T *str, const Number_T offset, Number_T &end_offset) noexcept {
+        using WhiteSpaceChars = WhiteSpaceChars_T<Char_T>;
 
-template <typename Char_T>
-static bool IsGreater(const Char_T *left, const Char_T *right, SizeT left_length, SizeT right_length,
-                      bool orEqual) noexcept {
-    SizeT offset = 0;
+        if (end_offset > offset) {
+            do {
+                --end_offset;
 
-    while ((left_length > offset) && (right_length > offset)) {
-        if (left[offset] < right[offset]) {
-            return false;
+                switch (str[end_offset]) {
+                    case WhiteSpaceChars::SpaceChar:
+                    case WhiteSpaceChars::LineControlChar:
+                    case WhiteSpaceChars::TabControlChar:
+                    case WhiteSpaceChars::CarriageControlChar:
+                        break;
+
+                    default: {
+                        ++end_offset;
+                        return;
+                    }
+                }
+
+            } while (end_offset > offset);
         }
-
-        if (left[offset] > right[offset]) {
-            return true;
-        }
-
-        ++offset;
     }
 
-    return (orEqual & (left_length == right_length));
-}
-
-template <typename Char_T>
-static bool IsEqual(const Char_T *left, const Char_T *right, SizeT length) noexcept {
-    SizeT offset = 0;
-
-    // if ((left != nullptr) && (right != nullptr)) {
-    while ((length > offset) && (left[offset] == right[offset])) {
-        ++offset;
-    }
-    // }
-
-    return (length == offset);
-}
-
-template <typename Char_T>
-static SizeT Hash(const Char_T *key, SizeT length) noexcept {
-    constexpr SizeT highest_bit = (SizeT{1} << ((sizeof(SizeT) * 8) - SizeT{1}));
-
-    SizeT hash   = SizeT{11};
-    SizeT base   = SizeT{33};
-    SizeT offset = 0;
-
-    while (offset < length) {
-        hash += SizeT(base * offset * SizeT(key[offset]));
-        base += offset;
-
-        if (offset != length) {
-            hash *= (length ^ offset);
-            base += offset;
-            --length;
-            hash += SizeT(key[length]);
+    // offset: the starting offset
+    // length: the number of characters
+    template <typename Char_T, typename Number_T>
+    static void Trim(const Char_T *str, Number_T &offset, Number_T &length) noexcept {
+        if (length != 0) {
+            Number_T end_offset = (length + offset);
+            TrimLeft(str, offset, end_offset);
+            TrimRight(str, offset, end_offset);
+            length = (end_offset - offset);
         }
-
-        ++offset;
     }
 
-    return (hash | highest_bit);
-}
-
-template <typename Char_T>
-static void ToLowerCase(Char_T *str, SizeT length) noexcept {
-    const Char_T *end = (str + length);
-
-    while (str < end) {
-        if ((*str >= 'A') && (*str <= 'Z')) {
-            *str += ' ';
-        }
-
-        ++str;
-    }
-}
-
-template <typename, SizeT32>
-struct HTMLSpecialChars_T {};
-
-template <typename StringStream_T, typename Char_T>
-static void EscapeHTMLSpecialChars(StringStream_T &stream, const Char_T *str, SizeT length) {
-    using HTMLSpecialChars = HTMLSpecialChars_T<Char_T, sizeof(Char_T)>;
-
-    if (QentemConfig::AutoEscapeHTML) {
+    template <typename Char_T>
+    static bool IsLess(const Char_T *left, const Char_T *right, SizeT left_length, SizeT right_length,
+                       bool orEqual) noexcept {
         SizeT offset = 0;
-        SizeT index  = 0;
 
-        while (index < length) {
-            switch (str[index]) {
-                case '&': {
-                    const SizeT   rem_length = (length - index);
-                    const Char_T *n_str      = (str + index);
+        while ((left_length > offset) && (right_length > offset)) {
+            if (left[offset] > right[offset]) {
+                return false;
+            }
 
-                    if ((rem_length > SizeT{5}) && (n_str[SizeT{5}] == HTMLSpecialChars::SemicolonChar)) {
-                        if (StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLQuote, SizeT{5}) ||
-                            StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLSingleQuote, SizeT{5})) {
-                            index += SizeT{6};
+            if (left[offset] < right[offset]) {
+                return true;
+            }
+
+            ++offset;
+        }
+
+        return (orEqual & (left_length == right_length));
+    }
+
+    template <typename Char_T>
+    static bool IsGreater(const Char_T *left, const Char_T *right, SizeT left_length, SizeT right_length,
+                          bool orEqual) noexcept {
+        SizeT offset = 0;
+
+        while ((left_length > offset) && (right_length > offset)) {
+            if (left[offset] < right[offset]) {
+                return false;
+            }
+
+            if (left[offset] > right[offset]) {
+                return true;
+            }
+
+            ++offset;
+        }
+
+        return (orEqual & (left_length == right_length));
+    }
+
+    template <typename Char_T>
+    static bool IsEqual(const Char_T *left, const Char_T *right, SizeT length) noexcept {
+        SizeT offset = 0;
+
+        // if ((left != nullptr) && (right != nullptr)) {
+        while ((length > offset) && (left[offset] == right[offset])) {
+            ++offset;
+        }
+        // }
+
+        return (length == offset);
+    }
+
+    template <typename Char_T>
+    static SizeT Hash(const Char_T *key, SizeT length) noexcept {
+        constexpr SizeT highest_bit = (SizeT{1} << ((sizeof(SizeT) * 8) - SizeT{1}));
+
+        SizeT hash   = SizeT{11};
+        SizeT base   = SizeT{33};
+        SizeT offset = 0;
+
+        while (offset < length) {
+            hash += SizeT(base * offset * SizeT(key[offset]));
+            base += offset;
+
+            if (offset != length) {
+                hash *= (length ^ offset);
+                base += offset;
+                --length;
+                hash += SizeT(key[length]);
+            }
+
+            ++offset;
+        }
+
+        return (hash | highest_bit);
+    }
+
+    template <typename Char_T>
+    static void ToLowerCase(Char_T *str, SizeT length) noexcept {
+        const Char_T *end = (str + length);
+
+        while (str < end) {
+            if ((*str >= 'A') && (*str <= 'Z')) {
+                *str += ' ';
+            }
+
+            ++str;
+        }
+    }
+
+    template <typename StringStream_T, typename Char_T>
+    static void EscapeHTMLSpecialChars(StringStream_T &stream, const Char_T *str, SizeT length) {
+        using HTMLSpecialChars = HTMLSpecialChars_T<Char_T, sizeof(Char_T)>;
+
+        if (QentemConfig::AutoEscapeHTML) {
+            SizeT offset = 0;
+            SizeT index  = 0;
+
+            while (index < length) {
+                switch (str[index]) {
+                    case '&': {
+                        const SizeT   rem_length = (length - index);
+                        const Char_T *n_str      = (str + index);
+
+                        if ((rem_length > SizeT{5}) && (n_str[SizeT{5}] == HTMLSpecialChars::SemicolonChar)) {
+                            if (StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLQuote, SizeT{5}) ||
+                                StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLSingleQuote, SizeT{5})) {
+                                index += SizeT{6};
+                                break;
+                            }
+                        }
+
+                        if ((rem_length > SizeT{4}) && (n_str[SizeT{4}] == HTMLSpecialChars::SemicolonChar) &&
+                            StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLAnd, SizeT{4})) {
+                            index += SizeT{5};
                             break;
                         }
-                    }
 
-                    if ((rem_length > SizeT{4}) && (n_str[SizeT{4}] == HTMLSpecialChars::SemicolonChar) &&
-                        StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLAnd, SizeT{4})) {
-                        index += SizeT{5};
+                        if ((rem_length > SizeT{3}) && (n_str[SizeT{3}] == HTMLSpecialChars::SemicolonChar)) {
+                            if (StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLLess, SizeT{3}) ||
+                                StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLGreater, SizeT{3})) {
+                                index += SizeT{4};
+                                break;
+                            }
+                        }
+
+                        stream.Write((str + offset), (index - offset));
+                        stream.Write(HTMLSpecialChars::HTMLAnd, HTMLSpecialChars::HTMLAndLength);
+                        ++index;
+                        offset = index;
                         break;
                     }
 
-                    if ((rem_length > SizeT{3}) && (n_str[SizeT{3}] == HTMLSpecialChars::SemicolonChar)) {
-                        if (StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLLess, SizeT{3}) ||
-                            StringUtils::IsEqual(n_str, HTMLSpecialChars::HTMLGreater, SizeT{3})) {
-                            index += SizeT{4};
-                            break;
-                        }
+                    case '<': {
+                        stream.Write((str + offset), (index - offset));
+                        stream.Write(HTMLSpecialChars::HTMLLess, HTMLSpecialChars::HTMLLessLength);
+                        ++index;
+                        offset = index;
+                        break;
                     }
 
-                    stream.Write((str + offset), (index - offset));
-                    stream.Write(HTMLSpecialChars::HTMLAnd, HTMLSpecialChars::HTMLAndLength);
-                    ++index;
-                    offset = index;
-                    break;
-                }
+                    case '>': {
+                        stream.Write((str + offset), (index - offset));
+                        stream.Write(HTMLSpecialChars::HTMLGreater, HTMLSpecialChars::HTMLGreaterLength);
+                        ++index;
+                        offset = index;
+                        break;
+                    }
 
-                case '<': {
-                    stream.Write((str + offset), (index - offset));
-                    stream.Write(HTMLSpecialChars::HTMLLess, HTMLSpecialChars::HTMLLessLength);
-                    ++index;
-                    offset = index;
-                    break;
-                }
+                    case '"': {
+                        stream.Write((str + offset), (index - offset));
+                        stream.Write(HTMLSpecialChars::HTMLQuote, HTMLSpecialChars::HTMLQuoteLength);
+                        ++index;
+                        offset = index;
+                        break;
+                    }
 
-                case '>': {
-                    stream.Write((str + offset), (index - offset));
-                    stream.Write(HTMLSpecialChars::HTMLGreater, HTMLSpecialChars::HTMLGreaterLength);
-                    ++index;
-                    offset = index;
-                    break;
-                }
+                    case '\'': {
+                        stream.Write((str + offset), (index - offset));
+                        stream.Write(HTMLSpecialChars::HTMLSingleQuote, HTMLSpecialChars::HTMLSingleQuoteLength);
+                        ++index;
+                        offset = index;
+                        break;
+                    }
 
-                case '"': {
-                    stream.Write((str + offset), (index - offset));
-                    stream.Write(HTMLSpecialChars::HTMLQuote, HTMLSpecialChars::HTMLQuoteLength);
-                    ++index;
-                    offset = index;
-                    break;
-                }
-
-                case '\'': {
-                    stream.Write((str + offset), (index - offset));
-                    stream.Write(HTMLSpecialChars::HTMLSingleQuote, HTMLSpecialChars::HTMLSingleQuoteLength);
-                    ++index;
-                    offset = index;
-                    break;
-                }
-
-                default: {
-                    ++index;
+                    default: {
+                        ++index;
+                    }
                 }
             }
-        }
 
-        stream.Write((str + offset), (length - offset));
-    } else {
-        stream.Write(str, length);
+            stream.Write((str + offset), (length - offset));
+        } else {
+            stream.Write(str, length);
+        }
     }
-}
+};
 
 // char
 template <typename Char_T>
@@ -385,7 +386,6 @@ struct HTMLSpecialChars_T<wchar_t, 2U> {
     static constexpr const wchar_t SemicolonChar = L';';
 };
 
-} // namespace StringUtils
 } // namespace Qentem
 
 #endif
